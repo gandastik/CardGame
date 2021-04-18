490b1710c6f92181afec41f96eb6821b8eb557fd
Kittiphon Singchom <bosszakittiphon@hotmail.com>
2021-04-18 22:06:11 +0700

    Add more card type in card collection

--- src/Classes/CardsCollection.java
+++ src/Classes/CardsCollection.java
@@ -29,20 +29,186 @@ public class CardsCollection {
     }
 
     public void addCardsToCollection(){
-        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 100, 1));
-        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 25, 120, 2));
-        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 100, 1));
-        cardsCollection.add(new FireTribe("Inari_Fire", 1, 30, 150, 3));
-        cardsCollection.add(new FireTribe("Burning_Algae", 1, 30, 150, 3));
-        cardsCollection.add(new FireTribe("Fire_Princess", 1, 30, 150,1));
-        cardsCollection.add(new FireTribe("Inari_Fire", 1, 30, 150, 3));
-        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 100, 1));
-        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 25, 120, 2));
-        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 100, 1));
-        cardsCollection.add(new FireTribe("Inari_Fire", 1, 30, 150, 3));
-        cardsCollection.add(new FireTribe("Burning_Algae", 1, 30, 150, 3));
-        cardsCollection.add(new FireTribe("Fire_Princess", 1, 30, 150, 1));
-        cardsCollection.add(new FireTribe("Inari_Fire", 1, 30, 150, 3));
+        //--------------------------------------LEVEL 1----------------------------------------------------
+        //Fire
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 1, 20, 150, 1));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 1, 40, 160, 2));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Inari_Fire", 1, 40, 170, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Burning_Algae", 1, 50, 150, 3));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+        cardsCollection.add(new FireTribe("Fire_Princess", 1, 20, 170,2));
+
+        //Rock
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",1,20,200,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",1,20,190,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",1,30,180,2));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Spherous_Lady",1,10,180,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",1,20,170,1));
+
+        //Water
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",1,20,150,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",1,40,110,3));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",1,20,160,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",1,30,100,4));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+        cardsCollection.add(new WaterTribe("The_Mermail",1,20,130,3));
+
+        //--------------------------------------LEVEL 2----------------------------------------------------
+        //Fire
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 2, 30, 200, 3));
+        cardsCollection.add(new FireTribe("Volcanic_Rat", 2, 30, 200, 3));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 2, 60, 210, 4));
+        cardsCollection.add(new FireTribe("Laval_Forest_Spirit", 2, 60, 210, 4));
+        cardsCollection.add(new FireTribe("Inari_Fire", 2, 60, 220, 5));
+        cardsCollection.add(new FireTribe("Inari_Fire", 2, 60, 220, 5));
+        cardsCollection.add(new FireTribe("Burning_Algae", 2, 60, 200, 5));
+        cardsCollection.add(new FireTribe("Burning_Algae", 2, 60, 200, 5));
+        cardsCollection.add(new FireTribe("Fire_Princess", 2, 30, 220,4));
+        cardsCollection.add(new FireTribe("Fire_Princess", 2, 30, 220,4));
+        cardsCollection.add(new FireTribe("Fire_Lavaval_Ignis", 2, 60, 260,10));
+        cardsCollection.add(new FireTribe("Laval_Judgment_Lord", 2, 60, 260,10));
+
+        //Rock
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",2,30,270,5));
+        cardsCollection.add(new RockTribe("Felix_King_Of_Battlefield",2,30,270,5));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",2,30,230,4));
+        cardsCollection.add(new RockTribe("Gem_Knight_Lapis",2,30,230,4));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",2,40,230,5));
+        cardsCollection.add(new RockTribe("Gem_Knight_Sardontyx",2,40,230,5));
+        cardsCollection.add(new RockTribe("Spherous_Lady",2,20,210,3));
+        cardsCollection.add(new RockTribe("Spherous_Lady",2,20,210,3));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",2,30,220,4));
+        cardsCollection.add(new RockTribe("Winged_Sphinx",2,30,220,4));
+        cardsCollection.add(new RockTribe("U.A._Blockbacker",2,50,300,12));
+        cardsCollection.add(new RockTribe("Vision_Hero_Gravito",2,30,310,11));
+
+        //Water
+        cardsCollection.add(new WaterTribe("Divine_Dragon",2,30,170,7));
+        cardsCollection.add(new WaterTribe("Divine_Dragon",2,30,170,7));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",2,50,140,6));
+        cardsCollection.add(new WaterTribe("Ice_Barrier",2,50,140,6));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",2,30,180,8));
+        cardsCollection.add(new WaterTribe("Lady_Of_The_Lake",2,30,180,8));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",2,40,150,8));
+        cardsCollection.add(new WaterTribe("Prince_Of_Ice",2,40,150,8));
+        cardsCollection.add(new WaterTribe("The_Mermail",2,30,160,7));
+        cardsCollection.add(new WaterTribe("The_Mermail",2,30,160,7));
+        cardsCollection.add(new WaterTribe("Ice_Master",2,40,190,13));
+        cardsCollection.add(new WaterTribe("Megician_Of_Prophecy",2,40,180,13));
     }
 
     //Methods
--- src/Classes/WaterTribe.java
+++ src/Classes/WaterTribe.java
@@ -1,4 +1,8 @@
 package Classes;
 
-public class WaterTribe {
+public class WaterTribe extends Card {
+    //Constructor
+    public WaterTribe(String name, int level, int damage, int hp, int cost) {
+        super(name,"water", level, damage, hp, cost);
+    }
 }
