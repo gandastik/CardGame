04f0d9b6b90f78b7eddfde90a47b3a2b163ffbc1
Your Name <you@example.com>
2021-04-21 01:13:22 +0700

    Try to set music but error to load music file

--- src/Controllers/OpenSceneController.java
+++ src/Controllers/OpenSceneController.java
@@ -1,20 +1,42 @@
 package Controllers;
 
+import Classes.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class OpenSceneController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OpenSceneController implements Initializable {
     @FXML
     private AnchorPane scenePane;
     private Scene scene;
     private Stage stage;
     private Parent root;
+
+    private Media soundOpenSceneBackgroundMedia;
+    private MediaPlayer soundOpenSceneBackgroundMediaPlayer;
+
+
+    //Initializations
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        playSoundOpenSceneBackground();
+    }
+
+
 
     public void onPlay(ActionEvent e) throws Exception{
         //Switch to Startscene
@@ -23,11 +45,21 @@ public class OpenSceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public void onQuit(ActionEvent e) throws Exception{
         stage = (Stage) scenePane.getScene().getWindow();
         System.out.println("You have quit the program!");
         stage.close();
+    }
+
+    private void playSoundOpenSceneBackground() {
+
+        soundOpenSceneBackgroundMedia = new Media("../Assets/SoundGame/SoundMenuMusic.mp3");
+
+        soundOpenSceneBackgroundMediaPlayer = new MediaPlayer(soundOpenSceneBackgroundMedia);
+        soundOpenSceneBackgroundMediaPlayer.play();
     }
 }
