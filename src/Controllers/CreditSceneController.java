e28742caef3b743519562a9d70891f7fa388f08f
Your Name <you@example.com>
2021-05-01 21:54:10 +0700

    AddCreditScene

---
+++ src/Controllers/CreditSceneController.java
@@ -0,0 +1,29 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreditSceneController  {
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void BackMainMenu(ActionEvent e) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Scenes/OpenScene.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
