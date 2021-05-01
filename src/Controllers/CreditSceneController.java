2197961ebe0bd84d74fe24af2fc638c1af18eb68
gandastik <gandastik@gmail.com>
2021-05-02 01:48:23 +0700

    add credit and change buyingPhase bg

--- src/Controllers/CreditSceneController.java
+++ src/Controllers/CreditSceneController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Classes.SoundEffects;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,19 +12,26 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CreditSceneController  {
-
-
+public class CreditSceneController  implements Initializable{
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void BackMainMenu(ActionEvent e) throws Exception {
+    private SoundEffects sfx;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.sfx = new SoundEffects();
+    }
+
+    public void onBack(ActionEvent e) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../Scenes/OpenScene.fxml"));
         root = loader.load();
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        this.sfx.playMenuClick();
     }
 }
